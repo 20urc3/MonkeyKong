27a898e475b4b8730768bb7caf3ef72cba2f2720
20urc3 <94982366+20urc3@users.noreply.github.com>
2021-11-28 18:27:02 +0100

    Update inputs.cpp

--- inputs.cpp
+++ inputs.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 Inputs::Inputs()
 {
-    std::cout << "JE SUIS LE CONSTRUCTEUR D'objet : Inputs" << endl;
-    //
+    // std::cout << "JE SUIS LE CONSTRUCTEUR D'objet : Inputs" << endl;
+
 }
 
 std::vector<string> Inputs::getAlphabet()
@@ -17,43 +17,35 @@ std::vector<string> Inputs::getAlphabet()
 std::string Inputs::create_random_inputs()
 {
     string results;
-    std::vector<std::vector<string>> ultraCombo;
-    ultraCombo.push_back(this->getAlphabet());
-    ultraCombo.push_back(this->hexa);
-    ultraCombo.push_back(this->numList);
-    ultraCombo.push_back(this->specialChar);
+    std::vector<std::vector<string>> finalInputs;
+    finalInputs.push_back(this->getAlphabet());
+    finalInputs.push_back(this->hexa);
+    finalInputs.push_back(this->numList);
+    finalInputs.push_back(this->specialChar);
 
-    std::vector<string> mesCouiilles = ultraCombo[0];
-    cout << "LA TAILLE - " << (ultraCombo[0]).size() << endl;
-
-    //cout << "LA TAILLE - " << mesCouiilles.size()<< endl;
-
+    std::vector<string> finalInputsString = finalInputs[0];
+    // cout << "Le inputs random généré est : " << (finalInputs[0]).size() << endl;
     
     int i = rand() % this->alphabet.size(); 
+    int j = rand() % this->hexa.size(); 
+    int k = rand() % this->numList.size();
+    int l = rand() % this->specialChar.size();
 
     std::stringstream ss;
-    //ss << " - "  << " <3";
-
-    //this->getID();
+    std::stringstream cc;
+    std::stringstream bb;
+    std::stringstream tt;
+    std::stringstream rr;
 
     ss << this->alphabet[i];
-    results = ss.str();
+    cc << this->hexa[j];
+    bb << this->numList[k];
+    tt << this->specialChar[l];
+    
+    rr << ss.str() << cc.str() << bb.str() << tt.str();
 
+    // cout << rr.str() << endl;
 
-    /*alphabet = alphabet[i]; //52
-
-    srand(time(0));
-    i = rand() % 10;
-    numList = numList[i]; // 10
-
-    srand(time(0));
-    i = rand() % 16;
-    hexa = hexa[i]; // 16
-
-    srand(time(0));
-    i = rand() % 10;
-    specialChar = specialChar[i]; // 10
-
-    random_inputs = specialChar + hexa + numList + alphabet;*/
+    results = rr.str();
     return results;
 }
