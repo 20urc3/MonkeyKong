c27f5071a59d0e10f96da6cedcdb7a3dff8a6c5b
20urc3 <94982366+20urc3@users.noreply.github.com>
2021-11-28 18:27:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,23 +15,17 @@ using namespace std;
 int main (int argc, char *argv[])
 {
     srand(time(0));
-    /*for(i = 0; i < 15000; i++){
-        printf("The random inputs generated is : %s", inputs::random_inputs)
-    }*/
-
+    
     Inputs myInputs;
     Inputs myInputs2;
-    Inputs myInputs3;
+    myInputs.create_random_inputs();
+    myInputs2.create_random_inputs();
 
-    myInputs3.create_random_inputs();
-
-
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 5000; i++)
     {
-        cout << myInputs.create_random_inputs() << endl;
+        cout << myInputs.create_random_inputs() << myInputs2.create_random_inputs() << endl;
+
     }
 
-
-    //printf("The random inputs generated is : %s", Inputs::random_inputs)
     return 0;
 }
